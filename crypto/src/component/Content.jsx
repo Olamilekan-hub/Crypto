d21d69edb387a3051c1533ed423996d152d4d6f9
Olamilekan <adebukolaolamilekan123@gmail.com>
2025-01-22 18:58:11 +0100

    updating

--- crypto/src/component/Content.jsx
+++ crypto/src/component/Content.jsx
@@ -3,12 +3,12 @@ const Content = () => {
     <div className="flex flex-col items-center justify-center gap-5 mt-5">
       <h1 className="text-4xl font-bold ">WHAT IS THE GOLDEN ERA OF CRYPTO?</h1>
       <div className="flex flex-col flex-grow gap-5 md:flex-row">
-      <p className="text-2xl font-semibold bg-gray-900 text-[gold] mx-3 py-4 px-12 rounded-xl text-center w-full">
+      <p className="text-2xl font-semibold bg-gray-900 text-[gold] md:mx-3 py-4 px-12 rounded-xl text-center w-full">
         The Golden Era of Crypto refers to a new phase of growth and development
         within the cryptocurrency and blockchain space that goes beyond the
         earlier periods of speculation, volatility, and hype.
       </p>
-      <p className="text-2xl font-semibold bg-gray-900 text-[gold] mx-3 py-4 px-12 rounded-xl text-center w-full">
+      <p className="text-2xl font-semibold bg-gray-900 text-[gold] md:mx-3 py-4 px-12 rounded-xl text-center w-full">
         This era is marked by greater institutional adoption, technological
         innovation, widespread mainstream awareness, and regulatory clarity that
         gives cryptocurrencies and decentralized finance (DeFi) a more solid
