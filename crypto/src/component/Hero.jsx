6832f1acc48ebb5d9b9d7a814ad279935d455b31
Olamilekan <adebukolaolamilekan123@gmail.com>
2025-01-23 03:02:37 +0100

    updating

--- crypto/src/component/Hero.jsx
+++ crypto/src/component/Hero.jsx
@@ -2,7 +2,7 @@ import BgImg from "../Assets/Image-1.jpg";
 
 const Hero = () => {
   return (
-    <div id="home" className="w-screen -z-2">
+    <div id="home" className=" -z-2">
       <img src={BgImg} alt="" className="object-contain w-full" />
     </div>
   );
